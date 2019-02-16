7b025e4b777bd98fd3b1761bfb24da14d7209cd9
Frank Fu <frankfu1122@gmail.com>
2019-02-16 16:05:52 +1100

    trial and error 1

--- dockerfiles-windows/vsbuildtools/installchoco.ps1
+++ dockerfiles-windows/vsbuildtools/installchoco.ps1
@@ -11,7 +11,7 @@ try {
   Write-Warning 'Unable to set PowerShell to use TLS 1.2 and TLS 1.1 due to old .NET Framework installed. If you see underlying connection closed or trust errors, you may need to do one or more of the following: (1) upgrade to .NET Framework 4.5 and PowerShell v3, (2) specify internal Chocolatey package location (set $env:chocolateyDownloadUrl prior to install or host the package internally), (3) use the Download + PowerShell method of install. See https://chocolatey.org/install for all install options.'
 }
 
-$env:chocolateyUseWindowsCompression = 'false'
+# $env:chocolateyUseWindowsCompression = 'false'
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 [System.Net.ServicePointManager]::SecurityProtocol = $securityProtocolSettingsOriginal
